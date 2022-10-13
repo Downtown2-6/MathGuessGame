2b67a66793baae5ee0fd47431c764a780642e899
Tamim <tamim@Melissas-MacBook-Pro.local>
2022-10-13 18:41:17 -0400

    all implementations working

--- README.md
+++ README.md
@@ -3,11 +3,13 @@
 ### This is not the full implementation 
 
 
-1. Still need to implement five guess maximum 
-2. Still need to implement reset button 
-3. Hints have been implemented but need work 
+~~1. Still need to implement five guess maximum~~
+~~2. Still need to implement reset button~~ 
+~~3. Hints have been implemented but need work~~ 
 
-- additionally, CSS styling is required 
+1. CSS styling is required 
+2. Clean up script.js (variables, less verbose)
+3. clean up index.html (change tags: button, form etc.)
 
 
 - functionally this product works 
--- script.js
+++ script.js
@@ -1,20 +1,42 @@
 // generating a random number between 1-100
 let y = Math.floor(Math.random() * 100 + 1);
+//let y = 55;   //<-- for testing purposes 
+let guessCounter = 1;
 
-let guess = 1;
-
+const input = document.getElementById('guessField');
 document.getElementById("submitGuess").onclick = function(){
-    let x = document.getElementById("guessField").value;
+    const x = document.getElementById("guessField").value;
 
     if (x == y){
-       document.getElementById('subText').innerHTML = "Congrats! You got it in " + guess + " guesses ";
-    }
-    else if( x > y){
-        guess++;
+       document.getElementById('subText').innerHTML = "Congrats! You got it in " + guessCounter + " guesses ";
+       document.getElementById('resetButton').disabled = false;
+    }else if( x > y && guessCounter < 5){
+        guessCounter++;
         document.getElementById('subText').innerHTML = "Try a smaller number!";
+        document.getElementById('resetButton').disabled = true;
+    }else if( x < y && guessCounter < 5){
+        guessCounter++;
+        document.getElementById('subText').innerHTML = "Try a larger number!";
+        document.getElementById('resetButton').disabled = true;
     }
     else{
-        guess++;
-        document.getElementById('subText').innerHTML = "Try a larger number!";
+        document.getElementById('subText').innerHTML = "Out of guesses! Try again"
+        document.getElementById('submitGuess').disabled = true;
+        document.getElementById('resetButton').disabled = false;
+
     }
 }
+document.getElementById('resetButton').onclick = function(){
+    document.getElementById('subText').innerHTML = "See if you can guess it!";
+    input.value = ' ';
+    document.getElementById('submitGuess').disabled = false;
+    y = Math.floor(Math.random() * 100 + 1);
+    guessCounter = 1;
+}
+
+
+//reset button will reset guess count, 
+//generate new number, 
+//set submit button to true, 
+//reset header text 
+
