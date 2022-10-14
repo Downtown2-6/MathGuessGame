2fce6c4fda508ebd87689f6e62a992313664ee96
Tamim <tamim@Melissas-MacBook-Pro.local>
2022-10-14 08:18:17 -0400

    fixed uncaught reference error

--- script.js
+++ script.js
@@ -3,32 +3,32 @@ let y = Math.floor(Math.random() * 100 + 1);
 //let y = 55;   //<-- for testing purposes 
 let guessCounter = 1;
 
-const input = document.getElementById('guessField');
-document.getElementById("submitGuess").onclick = function(){
+// const input = document.getElementById('guessField');
+document.getElementById("submitGuess").onclick = function () {
     const x = document.getElementById("guessField").value;
-
-    if (x == y){
-       document.getElementById('subText').innerHTML = "Congrats! You got it in " + guessCounter + " guesses ";
-       document.getElementById('resetButton').disabled = false;
-    }else if( x > y && guessCounter < 8){
+    console.log("checkpint");
+    if (x == y) {
+        document.getElementById('subText').innerHTML = "Congrats! You got it in " + guessCounter + " guesses ";
+        document.getElementById('resetButton').disabled = false;
+    } else if (x > y && guessCounter < 8) {
         guessCounter++;
         document.getElementById('subText').innerHTML = "Try a smaller number!";
         document.getElementById('resetButton').disabled = true;
-    }else if( x < y && guessCounter < 8){
+    } else if (x < y && guessCounter < 8) {
         guessCounter++;
         document.getElementById('subText').innerHTML = "Try a larger number!";
         document.getElementById('resetButton').disabled = true;
     }
-    else{
+    else {
         document.getElementById('subText').innerHTML = "Out of guesses! Try again"
         document.getElementById('submitGuess').disabled = true;
         document.getElementById('resetButton').disabled = false;
 
     }
 }
-document.getElementById('resetButton').onclick = function(){
+document.getElementById('resetButton').onclick = function () {
     document.getElementById('subText').innerHTML = "See if you can guess it!";
-    input.value = ' ';
+    // input.value = ' ';
     document.getElementById('submitGuess').disabled = false;
     y = Math.floor(Math.random() * 100 + 1);
     guessCounter = 1;
